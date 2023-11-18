4470de8d8f8e4fb973baf74b37e00057335550bb
nghia <nghiadam74@gmail.com>
2023-11-18 17:07:51 +0700

    bắt đầu dự án

---
+++ app/src/main/java/nghiadtph40607/fpoly/qlkh/MainActivity.java
@@ -0,0 +1,14 @@
+package nghiadtph40607.fpoly.qlkh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
